56931224caa1b51a08f2fa4a46c78b2b85a169f9
pinguet62 <pinguet62@gmail.com>
2013-11-03 19:19:54 +0100

    SpringSecurity: Token: Replacement of UserPassword by Abstract

--- src/main/java/fr/pinguet62/croquette/springsecurity/OAuthAuthenticationToken.java
+++ src/main/java/fr/pinguet62/croquette/springsecurity/OAuthAuthenticationToken.java
@@ -2,7 +2,7 @@ package fr.pinguet62.croquette.springsecurity;
 
 import java.util.Collections;
 
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
@@ -10,13 +10,11 @@ import fr.pinguet62.croquette.model.User;
 
 /**
  * Used to authenticate the user with OAuth. <br />
- * Simple extension of {@link UsernamePasswordAuthenticationToken}, where: <li>
- * <code>principal</code> is the <code>token</code>; <li>
- * <code>credentials</code> is null; <li> <code>detail</code> is the {@link User}
- * object; <li> {@link GrantedAuthority} is <code>ROLE_USER</code>.
+ * Simple extension of {@link AbstractAuthenticationToken}, where
+ * <code>detail</code> contains the {@link User} object, and
+ * {@link GrantedAuthority} is <code>ROLE_USER</code>.
  */
-public final class OAuthAuthenticationToken extends
-	UsernamePasswordAuthenticationToken {
+public final class OAuthAuthenticationToken extends AbstractAuthenticationToken {
 
     /** Auto-generated serial version UID. */
     private static final long serialVersionUID = -7960714936545188027L;
@@ -28,12 +26,23 @@ public final class OAuthAuthenticationToken extends
      *            The user token.
      */
     public OAuthAuthenticationToken(final String token) {
-	super(token, null, Collections.singleton(new SimpleGrantedAuthority(
-		"ROLE_USER")));
+	super(Collections.singleton(new SimpleGrantedAuthority("ROLE_USER")));
+
+	this.setAuthenticated(true);
 
 	User user = new User();
 	user.setToken(token);
 	this.setDetails(user);
+    }
+
+    @Override
+    public Object getCredentials() {
+	return null;
+    }
+
+    @Override
+    public Object getPrincipal() {
+	return null;
     }
 
 }
