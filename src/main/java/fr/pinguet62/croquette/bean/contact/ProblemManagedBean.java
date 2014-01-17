4d5970953563a696e688e3b94f6db9c78d99fa05
pinguet62 <pinguet62@gmail.com>
2014-01-17 21:11:55 +0100

    Contact : Managed bean + validation + template

---
+++ src/main/java/fr/pinguet62/croquette/bean/contact/ProblemManagedBean.java
@@ -0,0 +1,15 @@
+package fr.pinguet62.croquette.bean.contact;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean(name = "problemManagedBean")
+@SessionScoped
+public final class ProblemManagedBean extends ContactManagedBean {
+
+    @Override
+    public void send() {
+	// TODO
+    }
+
+}
---
+++ src/main/java/fr/pinguet62/croquette/bean/contact/SuggestionManagedBean.java
@@ -0,0 +1,15 @@
+package fr.pinguet62.croquette.bean.contact;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean(name = "suggestionManagedBean")
+@SessionScoped
+public final class SuggestionManagedBean extends ContactManagedBean {
+
+    @Override
+    public void send() {
+	// TODO
+    }
+
+}
