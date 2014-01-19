6bac842184881bd25eabf7592624c35fc8969474
pinguet62 <pinguet62@gmail.com>
2014-01-19 17:24:20 +0100

    Contact: confirm & reset input + bean scope + view update after button click

--- src/main/java/fr/pinguet62/croquette/bean/contact/ProblemManagedBean.java
+++ src/main/java/fr/pinguet62/croquette/bean/contact/ProblemManagedBean.java
@@ -1,15 +1,16 @@
 package fr.pinguet62.croquette.bean.contact;
 
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.SessionScoped;
+import javax.faces.bean.ViewScoped;
 
 @ManagedBean(name = "problemManagedBean")
-@SessionScoped
+@ViewScoped
 public final class ProblemManagedBean extends ContactManagedBean {
 
     @Override
     public void send() {
 	// TODO
+	confirmSending();
     }
 
 }
