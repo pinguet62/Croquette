fea407f437a8800837e1cd9c130fb4e4b0f61013
pinguet62 <pinguet62@gmail.com>
2014-01-23 21:59:06 +0100

    Outcome navigation

--- src/main/java/fr/pinguet62/croquette/oauth/LoginOAuthServlet.java
+++ src/main/java/fr/pinguet62/croquette/oauth/LoginOAuthServlet.java
@@ -11,7 +11,7 @@ import javax.servlet.http.HttpServletResponse;
 
 /** {@link Servlet} used to redirect to Google OAuth.<br /> */
 @WebServlet(urlPatterns = "/OAuth/login")
-public final class LoginOAuth extends HttpServlet {
+public final class LoginOAuthServlet extends HttpServlet {
 
     /** Auto generated serial version UID. */
     private static final long serialVersionUID = 751973283607034249L;
@@ -30,7 +30,7 @@ public final class LoginOAuth extends HttpServlet {
     protected void doGet(final HttpServletRequest request,
 	    final HttpServletResponse response) throws ServletException,
 	    IOException {
-	response.sendRedirect(LoginOAuth.url);
+	response.sendRedirect(LoginOAuthServlet.url);
     }
 
 }
